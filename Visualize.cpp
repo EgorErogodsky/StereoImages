c9d7e474b592738c66dc7004b90350828b04f3e1
Egor <77076136+EgorErogodsky@users.noreply.github.com>
2022-01-05 19:41:54 +0300

    Add files via upload

---
+++ Visualize.cpp
@@ -0,0 +1,11 @@
+﻿#include "Visualize.h"
+
+// Отображение облака точек.
+void Visualize(pcl::PointCloud<pcl::PointXYZRGB>::Ptr pointCloud)
+{
+	pcl::visualization::CloudViewer viewer ("Stereo image");
+	viewer.showCloud(pointCloud);
+	while (!viewer.wasStopped())
+	{
+	}
+}
