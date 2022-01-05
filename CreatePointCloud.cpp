959a0bbdd2aeca725cfee8e6b949fb8cb07745c0
Egor <77076136+EgorErogodsky@users.noreply.github.com>
2022-01-05 19:41:21 +0300

    Add files via upload

---
+++ CreatePointCloud.cpp
@@ -0,0 +1,33 @@
+﻿#include "CreatePointCloud.h"
+
+// Создание облака точек из карты глубины.
+// Фон не отображается.
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr CreatePointCloud(Mat disparityMap, Mat img)
+{
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr pointCloud(new pcl::PointCloud<pcl::PointXYZRGB>());
+	for (int i = 0; i < img.rows; i++)
+	{
+		for (int j = 0; j < img.cols; j++)
+		{
+			float z = disparityMap.at<float>(Point(j, i));
+			
+			if (z > 0)
+			{
+				pcl::PointXYZRGB point;
+
+				point.x = j - img.cols / 2;
+				point.y = img.rows - i - img.rows / 2;
+				point.z = z;
+
+				Vec3b channels = img.at<Vec3b>(Point(j, i)); //bgr
+				point.r = (uint8_t)channels[2];
+				point.g = (uint8_t)channels[1];
+				point.b = (uint8_t)channels[0];
+
+				pointCloud->points.push_back(point);
+			}
+		}
+	}
+
+	return pointCloud;
+}
---
+++ StereoImages.cpp
@@ -0,0 +1,23 @@
+﻿#include <pcl/visualization/cloud_viewer.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/mat.hpp>
+#include "ImageToPointCloud.h"
+#include "Visualize.h"
+#include "SplitImage.h"
+#include "GetDisparityMap.h"
+#include "CreatePointCloud.h"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char* argv[])
+{
+	string path = argv[1]; 
+	Mat img = imread(path, IMREAD_COLOR);
+
+	Mat disparityMap = GetDisparityMap(img);
+
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr pointCloud = CreatePointCloud(disparityMap, img);
+
+	Visualize(pointCloud);
+}
